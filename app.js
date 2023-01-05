951af26728ab33f1b8b42438cef935cb731bafa9
chrisvien <115595886+chrisvien@users.noreply.github.com>
2023-01-05 13:33:18 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const menu = document.queryselector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+} ) ;
+
+
